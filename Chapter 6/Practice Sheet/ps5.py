190e54cc9e99d8d8f464d3aba00e02aa5302acfc
Karthikkrth <krthkrth369@gmail.com>
2025-06-02 10:40:47 +0530

    Add files via upload

---
+++ Chapter 6/Practice Sheet/ps5.py
@@ -0,0 +1,23 @@
+nms = ["raju","krth","harun","Madhav"]
+
+nm = input("Enter name to search in the list : ")
+
+if (nm in nms):
+    print("Name is fouund in the list...")
+else:
+    print("Name is not found in the list")
+
+
+
+
+
+# names = ["Alice", "Bob", "Charlie", "David"]
+# name_index_map = {name: idx for idx, name in enumerate(names)}
+# name_to_check = input("Enter the name to check: ")
+
+# if name_to_check in name_index_map:
+#     print(f"The name is present at index {name_index_map[name_to_check]}.")
+# else:
+#     print("The name is not present in the list.")
+
+
