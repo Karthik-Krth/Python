1a47403129b2309dd5e867011a34b6b0ba9c6446
Karthikkrth <krthkrth369@gmail.com>
2025-06-04 11:13:30 +0530

    Add files via upload

---
+++ Chapter 8/defa_para.py
@@ -0,0 +1,7 @@
+def GD(nm,age=19): #default parameter
+    print(f"Very Good Morning {nm}")
+    print(f"Age of {nm} is {age}")
+
+GD("Krth")
+GD("Raju")
+GD("Harun",22)
---
+++ Chapter 8/fun.py
@@ -0,0 +1,7 @@
+def GD(nm,age):
+    print(f"Very Good Morning {nm}")
+    print(f"Age of {nm} is {age}")
+
+GD("Krth",19)
+GD("Raju",20)
+GD("Harun",20)
