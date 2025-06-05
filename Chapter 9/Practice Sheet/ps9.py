380e706be5298cca0f9780cd99df7e441b189593
Karthikkrth <krthkrth369@gmail.com>
2025-06-05 11:17:23 +0530

    Add files via upload

---
+++ Chapter 9/Practice Sheet/ps9.py
@@ -0,0 +1,9 @@
+with open("this.txt") as f :
+    c = f.read()
+with open("file.txt","r") as f :
+   c1 = f.read()
+
+if c == c1 :
+    print("Yes these files are Indentical ...")
+else :
+    print("Yes these files are Indentical ...")
