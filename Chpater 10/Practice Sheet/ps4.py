dfee5b1a1f2eebcf82d6775e4e893add1c17cc94
Karthikkrth <krthkrth369@gmail.com>
2025-06-06 06:48:34 +0530

    Add files via upload

---
+++ Chpater 10/Practice Sheet/ps4.py
@@ -0,0 +1,19 @@
+class cal :
+    def __init__(self,n):
+        self.n = n
+    @staticmethod
+    def greet():
+        print("Hello \"Mathematician\"")
+    def square(self):
+        print(f"Square of given number : {self.n*self.n}")
+    def cube(self):
+        print(f"Cube of given number : {self.n**3}")
+    def sq_root(self):
+        print(f"Square root of given number : {int(self.n**0.5)}")
+    
+
+a = cal(4)
+a.greet()
+a.sq_root()
+a.square()
+a.cube()
